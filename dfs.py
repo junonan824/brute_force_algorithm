364a930016d4cd701c6aaa60afbca7bec4041d82
junon an <gaebalnam824@gmail.com>
2024-08-07 22:41:14 +0900

    Implement depth-first search (DFS) algorithm

---
+++ dfs.py
@@ -0,0 +1,7 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start, end=' ')
+    for next in graph[start] - visited:
+        dfs(graph, next, visited)
