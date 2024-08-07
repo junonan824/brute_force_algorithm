e0334d26a8219e45504bf14a8fda7e19774f6b05
junon an <gaebalnam824@gmail.com>
2024-08-07 22:41:04 +0900

    Implement brute force algorithm to find all paths

---
+++ brute_force.py
@@ -0,0 +1,14 @@
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in graph:
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for p in newpaths:
+                path.append(p)
+    return paths
+
